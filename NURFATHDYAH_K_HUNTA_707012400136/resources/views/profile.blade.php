176016b1935240ba4897cc1730a4376588eeb101
dyahhunta <nurfathdyahkurniawatihunta@gmail.com>
2025-10-08 14:48:17 +0700

    Pratrikum Modul 2

---
+++ NURFATHDYAH_K_HUNTA_707012400136/resources/views/profile.blade.php
@@ -0,0 +1,3 @@
+<h1> Profile Page </h1>
+<p>Nama : Nur'Fathdyah.K.Hunta </p>
+<p>Jurusan : Sistem Informasi Kota Cerdas </p>
--- NURFATHDYAH_K_HUNTA_707012400136/routes/web.php
+++ NURFATHDYAH_K_HUNTA_707012400136/routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function () {
+    return 'Hello, Dyah';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return "Nama Saya $name";
+});
+
+Route::get('/greet/{name?}', function ($name = 'Guest') {
+    return "Halo, $name";
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Dyah', 'nim' => '707012400136', 'kelas' => 'D4SIKC 48-02']);
+});
+
+Route::get('/home', function () {
+    return 'Halo,Ini adalah halaman Home';
+})->name('home.page');
