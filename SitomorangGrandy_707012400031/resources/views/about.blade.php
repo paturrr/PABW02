021138ca7b420d15ec22b0940cfd38cae380260e
Grandy19 <grandysitumorang353@gmail.com>
2025-10-08 15:04:59 +0700

    Modul2

---
+++ SitomorangGrandy_707012400031/resources/views/about.blade.php
@@ -0,0 +1,7 @@
+<h1>About Page</h1>
+<p>Nama: {{ $name }}</p>
+<p>Nim: {{ $nim }}</p>
+<p>Kelas: {{ $kelas }}</p>
+
+
+<a href="{{ route('home.page')  }}">Kembali ke Home</a>
--- SitomorangGrandy_707012400031/routes/web.php
+++ SitomorangGrandy_707012400031/routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello',function() {
+    return 'Hello, Grandy'; 
+});
+
+Route::get('/user/{Grandy}',function ($name) {
+    return "Nama Saya $name";
+});
+
+Route::get('/greet/{name?}', function ($name = 'Guest') {
+    return "Halo, $name";
+});
+
+Route::get('/profile', function() {
+    return view('profile');
+});
+
+Route::get('/home', function () {
+    return 'Halo, Ini adalah halaman Home';
+})->name('home.page');
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Grandy', 'nim' => '707012400031', 'kelas' => 'D4SIKC4802']);
+});
