62dde26aefa9cc83863f0dbb36edd3e64f7a31b1
alvjnsk <alvinkurniawan936@gmail.com>
2025-10-08 14:47:25 +0700

    Modul-02

---
+++ Alvinmuhamadshabiqkurniawan_707012400048/resources/views/profile.blade.php
@@ -0,0 +1,3 @@
+<h1>Profile Page</h1>
+<p>Nama: Isi dengan nama anda.</p>
+<p>Jurusan: Isi dengan jurusan anda.</p>
--- Alvinmuhamadshabiqkurniawan_707012400048/routes/web.php
+++ Alvinmuhamadshabiqkurniawan_707012400048/routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function() {
+    return 'Hello, alvjn';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return "Nama saya $name";
+});
+
+Route::get('/greet/{name?}', function ($name = 'Guest') {
+    return "Halo $name";
+});
+
+Route::get('/profile', function(){
+    return view('profile');
+});
+
+Route::get('/about', function() {
+    return view('about', ['name' => 'alvjn', 'nim' =>'707012400048', 'kelas' => '48-02']);
+});
+
+Route::get('/home', function () {
+    return ('Halo ini adalah halaman home');
+})->name('home page');
