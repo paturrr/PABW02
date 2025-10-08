8d771f74ff1af1e842105662e4a884eb47d12899
Rakamp0 <rakamahesta97@gmail.com>
2025-10-08 15:02:23 +0700

    MODUL02

--- RakaMahestaPratama_707012400016/routes/web.php
+++ RakaMahestaPratama_707012400016/routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function() {
+    return 'Hello, Raka Mahesta';
+});
+
+Route::get('/user/{Raka}', function($name) {
+    return "Nama saya $name";
+});
+
+Route::get('/greet/{Raka?}', function($name = 'guest') {
+    return "Nama saya $name";
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Raka Mahesta Pratama', 'nim' => '707012400016', 'class' => 'D4SIKC-48-02']);
+});
+
+Route::get('/home', function () {
+    return 'Hallo, ini adalah halaman home';
+})->name('home.page');
