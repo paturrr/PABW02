20a844a5d37796b722096ce416e16a8a05dbed21
andidelarezky <andidelarezky@gmail.com>
2025-10-08 14:58:19 +0700

    praktikum modul 2

---
+++ AndiDelaRezky_707012400147/resources/views/profile.blade.php
@@ -0,0 +1,3 @@
+<h1>Profile Page</h1>
+<p>Nama     : Andi Dela Rezky.</p>
+<p>Jurusan  : SIKC.</p>
--- AndiDelaRezky_707012400147/routes/web.php
+++ AndiDelaRezky_707012400147/routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello, Andi dela rezky';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return "Nama Saya $name";
+});
+
+Route::get('/greet/{name?}', function ($name  = 'guest') {
+    return "Halo $name";
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/about', function () {
+    return view('about' , ['name' => 'Andi Dela Rezky', 'nim' => '707012400147', 'kelas' => 'D4 SIKC-48-02']);
+});
+
+Route::get('/home', function () {
+    return 'Hello,ini adalah halaman home';
+}) ->name('home.page');
