50072fca93e29d8868357d420d5a87c10df91bc0
RarastW <ariqnayaka08@gmail.com>
2025-10-08 13:24:54 +0700

    modul-02

---
+++ AriqAhmadNayaka_707012400008/resources/views/panggilnama.blade.php
@@ -0,0 +1,5 @@
+<div>
+    nama : {{$nama}}
+    <br>
+    <a href="{{ route('home.page') }}">Kembali ke Home</a>
+</div>
--- AriqAhmadNayaka_707012400008/routes/web.php
+++ AriqAhmadNayaka_707012400008/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/nama/{nama}', function ($nama) {
+    return view('panggilnama', ['nama' => $nama]);
+});
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home.page');
