b691bfab676fa0ae2e3165f8ed8775420b573697
Muhammad Raihan Abdul Muhith <raihanam255@gmail.com>
2025-10-08 14:48:12 +0700

    Modul 2

---
+++ MuhammadRaihanAbdulMuhith_707012400098/resources/views/profile.blade.php
@@ -0,0 +1,3 @@
+<h1>Profile Page</h1>
+<p>Nama    : Muhammad Raihan Abdul Muhith</p>
+<p>Jurusan : D4 Sistem Informasi Kota Cerdas</p>
--- MuhammadRaihanAbdulMuhith_707012400098/routes/web.php
+++ MuhammadRaihanAbdulMuhith_707012400098/routes/web.php
@@ -5,3 +5,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello, Muhith!';
+});
+
+Route::get('/user/{Muhith}', function ($name) {
+    return "Nama Saya $name";
+});
+
+Route::get('/greet/{name?}', function ($name = 'Guest') {
+    return "Halo, $name";
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Muhammad Raihan Abdul Muhith', 
+    'nim' => '707012400098', 'kelas' => 'D4 SIKC 48-02']);
+});
+
+Route::get('/home', function () {
+    return 'Halo, Ini adalah halaman Home';
+})->name('home.page');
