b05690dc8498f3949a964ca3a54a95274c16dec6
Suci Ramadhani <sucii.ramadhanii17@gmail.com>
2025-10-08 14:47:20 +0700

    PABW praktikum modul 2

--- Suci_Ramadhani_707012400027/routes/web.php
+++ Suci_Ramadhani_707012400027/routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'hello, suci ramadhani';
+ });
+
+ Route::get('/greet/{name?}', function ($name = 'Anomali') {
+    return "Halo $name";
+ });
+
+  Route::get('/user/{name?}', function ($name = 'Suci') {
+    return "Halo $name";
+ });
+
+ Route::get('/profile', function () {
+    return view ('profile');
+ });
+
+ Route::get('/about', function () {
+    return view ('about', ['name' => 'Suci Ramadhani', 'nim' => '202012400027' , 'kelas' => 'D4SIKC-48-02']);
+ });
+
+ Route::get('/home', function () {
+    return 'Halo, ini adalah halaman Home!';
+ })-> name ('home.page');
