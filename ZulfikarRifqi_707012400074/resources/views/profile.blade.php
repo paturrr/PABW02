eda100d8e13199eb950683b14c363a45091c8e18
Jukipok <zulfikarrifqi2504@gmail.com>
2025-10-08 14:53:46 +0700

    Praktikum Modul 2

---
+++ ZulfikarRifqi_707012400074/resources/views/profile.blade.php
@@ -0,0 +1,3 @@
+<h1>Profile Page</h1>
+<p>Nama     : Zulfikar Rifqi</p>
+<p>Jurusan  : Sistem Informasi Kota Cerdas</p>
--- ZulfikarRifqi_707012400074/routes/web.php
+++ ZulfikarRifqi_707012400074/routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function(){
+    return 'Hello, Zulfikar';
+});
+
+Route::get('/user/{Zulfikar}', function ($name){
+    return "Nama Saya $name";
+});
+
+Route::get('/great/{Zulfikar}', function  ($name = 'Guest'){
+    return "Hallo, $name";
+});
+
+Route::get('/profile', function (){
+    return view('profile');
+});
+
+Route::get('/about', function (){
+    return view('about', ['name' => 'Zulfikar Rifqi', 'NIM' => '707012400074', 'Kelas' => 'D4 SIKC 48-02']);
+});
+
+Route::get('/home', function (){
+    return 'Halo, ini adalah halaman Home';
+})->name('home.page');
