9557d363d8810c044f05ea7ad53f2bec21f944d0
ariekzakib <zakibariek@gmail.com>
2025-10-08 15:05:25 +0700

    Modul02

---
+++ AriekZakiBadran_707012400065/resources/views/profile.blade.php
@@ -0,0 +1,3 @@
+<h1>Profile Page</h1>
+<p>Nama : Ariek Zaki Badran</p>
+<p>Nim  : 707012400065</p>
--- AriekZakiBadran_707012400065/routes/web.php
+++ AriekZakiBadran_707012400065/routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello, Ariek';
+});
+
+Route::get('/user/{AriekZakiBadran}', function ($name) {
+    return "Nama Saya $name";
+});
+
+Route::get('/greet/{name?}', function ($name = 'Guest') {
+    return "Halo, $name";
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Ariek Zaki Badran', 'nim' => '707012400065', 'kelas' => 'D4SIKC48-02']);
+});
+
+Route::get('/home', function () {
+    return 'Halo, ini adalah halaman Home';
+})->name('home.page');
