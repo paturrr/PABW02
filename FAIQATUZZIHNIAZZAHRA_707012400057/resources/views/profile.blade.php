318ddd7b4eba6d33faee4c40a737f0c54f70be81
faiqatuzzihniiaz <faiqatuzzihniiaz@gmail.com>
2025-10-08 14:45:14 +0700

    Praktikum Modul 2

---
+++ FAIQATUZZIHNIAZZAHRA_707012400057/resources/views/profile.blade.php
@@ -0,0 +1,3 @@
+<h1> Profile Page </h1>
+<p> Nama    : Faiqatuzzihni Az Zahra </p>
+<p> Jurusan : S1 Terapan Sistem Informasi Kota Cerdas </p>
--- FAIQATUZZIHNIAZZAHRA_707012400057/routes/web.php
+++ FAIQATUZZIHNIAZZAHRA_707012400057/routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello, nama';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return "Nama saya $name";
+});
+
+Route::get('/greet/{name}', function ($name = 'Naura') {
+    return "Halo, $name";
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Faiqatuzzihni Az Zahra', 'nim' => '707012400057', 'kelas' => 'D4SIKC-48-02']);
+});
+
+Route::get('/home', function () {
+    return 'Halo, Ini adalah halaman Home';
+}) ->name('home.page');
