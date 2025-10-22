6a05f4c697d5039b2d5b35303d2289f81d7a560e
Ahmad Fathurrohman <ahmad.fathurrohman26@gmail.com>
2025-10-22 12:35:41 +0700

    remove

--- RainierPutraGunawan_707012400059/resources/views/about.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>About page</h1>
-<p>Nama : {{ $name }}</p>
-<p>Nim : {{ $nim }}</p>
-<p>Class : {{ $class }}</p>
-
-<a href= "{{route ('home.page')}}">Kembali ke home</a>
--- RainierPutraGunawan_707012400059/resources/views/profile.blade.php
+++
@@ -1,3 +0,0 @@
-<h1>Profile Page</h1>
-<p>nama : isi dengan nama anda</p>
-<p>Jurusan : isi dengan jurusan anda</p>
