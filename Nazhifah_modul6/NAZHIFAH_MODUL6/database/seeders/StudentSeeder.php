c8a62fa893ac4cc78eccf38b159548bc77220806
nazhifahnuriyahwahdani-byte <nazhifahnuriyahwahdani@gmail.com>
2025-11-12 14:50:41 +0600

    NazhifahNuriyahW_Modul6

---
+++ Nazhifah_modul6/NAZHIFAH_MODUL6/database/seeders/StudentSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class StudentSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $faker = Faker::create('id_ID');
+
+        for ($i = 0; $i < 20; $i++) {
+            DB::table('mahasiswa')->insert([
+                'nama' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'jurusan' => $faker->randomElement(['Informatika', 'Sistem Informasi', 'Teknik Komputer']),
+                'umur' => $faker->numberBetween(18, 25),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
